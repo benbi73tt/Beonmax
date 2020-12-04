5ae42d7c7784874970dd6d28af921815e7704e18
Anton <bur2000_2010@mail.ru>
2020-12-04 09:34:58 +0400

    тесты

--- beonmax_5.Бонус/script.js
+++ beonmax_5.Бонус/script.js
@@ -10,3 +10,35 @@
 // mocha - библиотеки для тестирования
 // npm install chai
 //npm install --global mocha
+
+function sayName(name) {
+    let message = `My name is ${name}`;
+    return message;
+}
+
+let arr = [1, 2, 3, 4, -5, 3, -11];
+let result = arr.reduce(function(sum, elem) {
+    return sum + elem;
+})
+
+let assert = require('chai').assert;
+
+describe("sayName", function() {
+    it("Получаем фразу с новым именем", function() {
+        assert.typeOf(sayName("alex"), 'string');
+    })
+});
+//!ЕСЛИ ОШИБКА в доступе
+// powershell от имени админа и вводим Get-ExecutionPolicy
+//Set-ExecutionPolicy unrestricted
+
+//mocha script	script-название файла
+//mocha --watch  - для автопроверки, если изменияем тест(если название не test), тогда после mocha ставим название файла
+//ctrl+c для выхода
+describe("arr", function() {
+    it("Получаем сумму чисел массива", function() {
+        assert.equal(result, -3);
+    })
+});
+
+//далее в проекте beon3
